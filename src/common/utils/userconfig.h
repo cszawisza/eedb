99446239f1f485c8b243962adcbd5b5e1d49fea8
cszawisza <cszawisza@gmail.com>
2015-06-07 11:58:55 +0200

    Add gtest and GoogleMock

--- src/common/utils/userconfig.h
+++ src/common/utils/userconfig.h
@@ -1,3 +1,23 @@
  #pragma once
 
-#include "QJson"
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+
+
+namespace eedb {
+namespace utils {
+
+using namespace std;
+
+class UserConfig
+{
+    QString toString() const;
+    string toStdString() const;
+
+private:
+QJsonObject m_config;
+};
+
+}
+}
