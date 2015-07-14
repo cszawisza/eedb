fd50a100c2b7f483344b312374a2e55c57ce3f3c
cszawisza <cszawisza@gmail.com>
2015-07-14 17:07:55 +0200

    remove old code

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,37 +4,11 @@
 #include <QAbstractSocket>
 #include "core/server.h"
 
-
 int keepassx_main_impl(int argc, char *argv[], int (*app_run)() )
 {
     QCoreApplication a(argc, argv);
-
-    QCoreApplication::setApplicationName(QStringLiteral("EKATALOG"));
-    QCoreApplication::setOrganizationName(QStringLiteral("BAPP"));
-
-    QFile *prog;
-    QString *msg;
-
-    prog = new QFile(argv [0]);
-    msg = new QString();
-
-    msg->append(QStringLiteral("Program ma wielkość "));
-    msg->append(QString::number(prog->size()));
-    msg->append(QStringLiteral(" bajtów, czyli "));
-    msg->append(QString::number((double)prog->size()/1024));
-    msg->append(QStringLiteral(" kilobajtów\n"));
-    msg->append(QStringLiteral("Został skompilowany w dniu "));
-    msg->append(QStringLiteral(__DATE__));
-    msg->append(QStringLiteral(" o godzinie "));
-    msg->append(QStringLiteral(__TIME__));
-    msg->append(QStringLiteral("\nna kompilatorze GCC w wersji "));
-    msg->append(QStringLiteral(__VERSION__));
-    msg->append(QStringLiteral("\nkorzystając z QT w wersji "));
-    msg->append(QT_VERSION_STR);
-    qDebug() << msg->toUtf8();
-
-    delete prog;
-    delete msg;
+    QCoreApplication::setApplicationName(QStringLiteral("eedb"));
+    QCoreApplication::setOrganizationName(QStringLiteral("eedb"));
 
     EEDB server;
     try{
